86aa5ba0105ce2985a18215afc1128a1ef29db76
Yutaka HARA <yutaka.hara+github@gmail.com>
2019-03-30 15:41:35 +0900

    feat: Add my_page to attendee mode

--- app/assets/javascripts/ovto_app.js.rb
+++ app/assets/javascripts/ovto_app.js.rb
@@ -20,13 +20,14 @@ class OvtoApp < Ovto::App
 
   class State < Ovto::State
     item :presenter_page, default: 0
+    item :my_page, default: 0
     item :mode, default: nil
-    item :slides, default: nil
 
     def presenter?; self.mode == "presenter"; end
+    def screen?; self.mode == "screen"; end
+    def atendee?; self.mode == "atendee"; end
 
-    def slide
-      page = self.presenter_page
+    def get_slide(page)
       `window.Opal.OvtoApp.slides[page]`
     end
   end
@@ -35,9 +36,9 @@ class OvtoApp < Ovto::App
     def on_keydown(event:)
       case event.JS['key']
       when "ArrowRight"
-        actions.presenter_next_page()
+        actions.next_page()
       when "ArrowLeft"
-        actions.presenter_prev_page()
+        actions.prev_page()
       end
       nil
     end
@@ -47,28 +48,49 @@ class OvtoApp < Ovto::App
       return {mode: mode}
     end
 
+    def next_page(state:)
+      state.presenter? ? actions.presenter_next_page : actions.my_next_page
+    end
+
+    def prev_page(state:)
+      state.presenter? ? actions.presenter_prev_page : actions.my_prev_page
+    end
+
     def presenter_prev_page(state:)
       if state.presenter_page > 0
         actions.update_presenter_page(page: state.presenter_page - 1)
       end
-      return nil
     end
 
     def presenter_next_page(state:)
       if state.presenter_page < (OvtoApp.slides.length-1)
         actions.update_presenter_page(page: state.presenter_page + 1)
       end
-      return nil
     end
 
     def update_presenter_page(page:)
       `App.presentation.send_action("set_presenter_page", {page: #{page}})`
       actions.set_presenter_page(page: page)
-      return nil
     end
 
     def set_presenter_page(page:)
       return {presenter_page: page}
+    end
+
+    def my_prev_page(state:)
+      if state.my_page > 0
+        {my_page: state.my_page - 1}
+      else
+        nil
+      end
+    end
+
+    def my_next_page(state:)
+      if state.my_page < (OvtoApp.slides.length-1)
+        {my_page: state.my_page + 1}
+      else
+        nil
+      end
     end
   end
 
@@ -76,7 +98,8 @@ class OvtoApp < Ovto::App
     def render(state:)
       o '.MainComponent' do
         o StateInspector
-        o PageControl if state.presenter?
+        o PageControl unless state.screen?
+        o MySlide if state.atendee?
         o Screen
       end
     end
@@ -97,7 +120,7 @@ class OvtoApp < Ovto::App
     class Screen < Ovto::Component
       def render(state:)
         o '.Screen', style: {border: "1px solid black"} do
-          o Slide, slide: state.slide
+          o Slide, slide: state.get_slide(state.presenter_page)
         end
       end
     end
@@ -109,21 +132,34 @@ class OvtoApp < Ovto::App
       end
     end
 
+    class MySlide < Ovto::Component
+      def render(state:)
+        style = {
+          border: "1px solid black",
+          background: "#eee",
+        }
+        # Inject js VDOM obj
+        o '.MySlide', {style: style}, state.get_slide(state.my_page)
+      end
+    end
+
     class PageControl < Ovto::Component
       def render(state:)
         o '.PageControl' do
           o 'input', {
             type: 'button',
             value: '<',
-            onclick: ->{ actions.presenter_prev_page() }
+            onclick: ->{ actions.prev_page() }
           }
           o 'input', {
             type: 'button',
             value: '>',
-            onclick: ->{ actions.presenter_next_page() }
+            onclick: ->{ actions.next_page() }
           }
         end
       end
     end
   end
 end
+
+#Ovto.debug_trace = true
