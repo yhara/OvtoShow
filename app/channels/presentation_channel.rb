6b1f355d19cc4b98dad733d0825e7ecd3b60beb2
Yutaka HARA <yutaka.hara+github@gmail.com>
2019-04-16 01:40:45 +0900

    cleanup: Remove unused code

--- app/channels/presentation_channel.rb
+++ app/channels/presentation_channel.rb
@@ -8,17 +8,7 @@ class PresentationChannel < ApplicationCable::Channel
   end
 
   def send_action(data)
+    # TODO: whitelist?
     ActionCable.server.broadcast('the_channel', data)
-  end
-
-  def select_page(data)
-    #PresentationChannel.broadcast_to("the_channel", "hi")
-    ActionCable.server.broadcast('the_channel', data.inspect)
-  end
-
-  def send_emo
-  end
-
-  def send_comment
   end
 end
