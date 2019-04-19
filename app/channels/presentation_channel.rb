e6f77d7ffc4ead6fd2794a483b339652b278e30f
Yutaka HARA <yutaka.hara+github@gmail.com>
2019-04-19 13:16:33 +0900

    security: Only whitelisted actions should go

--- app/channels/presentation_channel.rb
+++ app/channels/presentation_channel.rb
@@ -7,11 +7,13 @@ class PresentationChannel < ApplicationCable::Channel
     # Any cleanup needed when channel is unsubscribed
   end
 
+  ALLOWED_ACTIONS = %w(receive_emo)
+  ADMIN_ONLY_ACTIONS = %w(set_presenter_page)
   def send_action(data)
-    # Admin-only actions
-    if data['ovto_action'] == "set_presenter_page"
-      return unless user
+    action_name = data['ovto_action']
+    if ALLOWED_ACTIONS.include?(action_name) ||
+        (ADMIN_ONLY_ACTIONS.include?(action_name) && user)
+      ActionCable.server.broadcast('the_channel', data)
     end
-    ActionCable.server.broadcast('the_channel', data)
   end
 end
